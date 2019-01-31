3132c6fc2a49e6a9b825947bff0c108742c26586
allenhu12 <allenhu12@gmail.com>
2019-01-31 12:42:12 +0800

    add the comments for how to get a dictionary from input and how to get the type name of the object

--- micro_major_by_163/2_python_basic/quiz_2/re_dict.py
+++ micro_major_by_163/2_python_basic/quiz_2/re_dict.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 a = input()
+#这里仅用到了字典的简单操作，请理解eval()直接获得字典变量的方法
 dict_a = eval(a)
-
+print(dict_a)
+#type(obj.__name__) can get the type of the object
 if type(dict_a).__name__ == 'dict':
     for key, value in dict_a.items():
         print("{}:{}".format(value,key))
